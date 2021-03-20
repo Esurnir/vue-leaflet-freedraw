0f05e8a1b9081d86f6a079bccb1921f0609a9b59
Jean-Baptiste Zeller <esurnir@gmail.com>
2021-03-20 19:31:02 +0100

    style: remove last few 'errors' from eslint

--- src/vue-leaflet.d.ts
+++ src/vue-leaflet.d.ts
@@ -1,5 +1,6 @@
 declare module '@vue-leaflet/vue-leaflet' {
   import { DefineComponent } from 'vue';
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   type VueComponent = DefineComponent<{}, {}, any>;
   export const LMap: VueComponent;
   export const LTileLayer: VueComponent;
